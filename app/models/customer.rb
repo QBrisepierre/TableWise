70ea4be79b4e6d566bfcc15de02649c0cc4e393b
Quentin Brisepierre <Quentin.brisepierre@protonmail.com>
2024-01-28 15:05:35 +0100

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ApplicationRecord
   has_many :restaurants, through: :no_shows
 
   validates :name, :phone, :email, presence: true
-  validates :phone, :email, uniqueness: { case_sensitive: false }
+  validates :phone, uniqueness: { case_sensitive: false }
 
   include PgSearch::Model
   pg_search_scope :search_by_email_and_phone,
