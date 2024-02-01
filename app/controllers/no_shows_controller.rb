087bc00e816f3df2dda4d5fb1084a41b32b4a3dc
Quentin Brisepierre <Quentin.brisepierre@protonmail.com>
2024-02-01 12:34:57 +0100

    refactoring dashboard section. Refacto restaurants controller. Table ordered by most recent no_show by date_service

--- app/controllers/no_shows_controller.rb
+++ app/controllers/no_shows_controller.rb
@@ -1,16 +1,2 @@
 class NoShowsController < ApplicationController
-
-  def create
-    raise
-    @no_show = NoShow.new(no_show_params)
-    @no_show.save
-
-    redirect_to dashboard_restaurant_path(current_user.restaurant_ids.first)
-  end
-
-  private
-
-  def no_show_params
-    params.require(:customer).permit(:customer_id, :restaurant_id, :date_service)
-  end
 end
